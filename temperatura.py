c76377f0b2935d38795505995cd43a7a1f220f3e
JhonatanGARCIAXD <Jhonatangarcia@colegioguanenta.edu.co>
2022-04-21 17:49:53 -0500

    Desarrollo: creacion diagrama de flujo

---
+++ temperatura.py
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+
+# imput 
+c=int(input("Digite el valor de los grados celsius: "))
+
+
+# precesing 
+k=c+273.15
+f=1.8*c+32
+
+
+# output
+print(str(c) + " grados celsius"  + " en kelvin es " + str(k) + " y en Farenheit " + str(f))
